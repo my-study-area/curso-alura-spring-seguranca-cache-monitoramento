f45f69d4139ee3ca4655e390611af1f37f24b8bc
Adriano Avelino <adriano-st777@hotmail.com>
2020-12-12 21:54:51 -0300

    inicia configuração para controle de autenticação

--- src/main/java/br/com/alura/forum/config/validacao/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/validacao/SecurityConfigurations.java
@@ -21,7 +21,9 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.antMatchers(HttpMethod.GET, "/topicos").permitAll();
+		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
+		.anyRequest().authenticated()
+		.and().formLogin();
 	}
 	
 	//Configuracoes de recursos estaticos (js, css, imagens e etc)
