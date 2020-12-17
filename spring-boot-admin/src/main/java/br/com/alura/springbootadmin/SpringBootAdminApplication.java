a8fc63327fbf78950d75f88e9f7ccba8b628c7d7
Adriano Avelino <adriano-st777@hotmail.com>
2020-12-17 20:44:35 -0300

    atualiza projeto do springbootadmin para versão 8 do java

--- spring-boot-admin/src/main/java/br/com/alura/springbootadmin/SpringBootAdminApplication.java
+++ spring-boot-admin/src/main/java/br/com/alura/springbootadmin/SpringBootAdminApplication.java
@@ -2,11 +2,11 @@ package br.com.alura.springbootadmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
-@Configuration
+@SpringBootApplication
 @EnableAutoConfiguration
 @EnableAdminServer
 public class SpringBootAdminApplication {
